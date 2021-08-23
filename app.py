40c41af20381a692e1faac57ea1367c90452fdef
Chris Penn <82205659+chrisp26@users.noreply.github.com>
2021-08-23 16:25:53 +0800

    Updated with completed Flask queries

--- app.py
+++ app.py
@@ -49,6 +49,9 @@ def welcome():
         f"/api/v1.0/<start>/<end>"
     )
 
+
+#-------------------------------------------------
+
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # Create our session (link) from Python to the DB
@@ -58,18 +61,67 @@ def precipitation():
     results = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date > '2016-08-23').\
             order_by(Measurement.date).all()
+
+    session.close()
+
     # Convert the query results to a dictionary using date as the key and prcp as the value.
     precipitations = []
 
-    for result in results:
+    for date, prcp in results:
             row = {}
-            row["date"] = result[0]
-            row["prcp"] = float(result[1])
+            row["date"] = date
+            row["prcp"] = float(prcp)
             precipitation.append(row)
 
     return jsonify(precipitations)
 
+#-------------------------------------------------
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    results = session.query(Stations.station, Stations.name).all()
+
     session.close()
+
+    # Convert the query results to a dictionary using date as the key and prcp as the value.
+    station_names = []
+
+    for station, name in results:
+            row = {}
+            row["Station ID"] = station
+            row["Station Name"] = name
+            station_names.append(row)
+
+    return jsonify(station_names)
+
+#-------------------------------------------------
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= start_date).all()
+
+    session.close()
+
+    # Convert the query results to a dictionary using date as the key and prcp as the value.
+    tobs_data = []
+
+    for date, tobs in results:
+            row = {}
+            row["Date"] = date
+            row["Temperature"] = tobs
+            tobs_data.append(row)
+
+    return jsonify(tobs_data)
+
+#-------------------------------------------------
+
 
     # Convert list of tuples into normal list
     all_names = list(np.ravel(results))
